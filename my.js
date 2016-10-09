d4f482b420b5071c4c13b1b4b2b024c88573a5b9
Andrea <andrea.mauri87@gmail.com>
2016-10-09 17:38:29 +0200

    starting to add the communication with the rsp
    register stream
    register query

--- my.js
+++ my.js
@@ -1,6 +1,10 @@
 
-var data = [];
+var rspAddress = "";
+var queryAddress = "";
+var sgraphAddress = "http://131.175.141.249/TripleWave-transform/sgraph";
+
 var initStream = function () {
+    var data = [];
 
 
     //var twAddress = "ws://131.175.141.249/TripleWave-transform/ws/stream";
@@ -19,7 +23,54 @@ var initStream = function () {
         data.push(message.data)
         $stream[0].value=data.join('\n');
     })
-
+    
+    registerStream();
     
 };
 
+
+var registerStream = function(){
+
+    $.ajax({
+        url:rspAddress,
+        method:'PUT',
+        headers:{
+            "Cache":"no-cache"
+        },
+        data:{
+            "streamIri":sgraphAddress
+        }
+    })
+    .done(function(response){
+        $('#response').html(response);
+    })
+    .fail(function( jqXHR, textStatus){
+        var error = "Error: " +textStatus+"\nPlease raise you hand and wait for a tutor";
+        $('#response').html(error);
+
+    })
+}
+
+
+var registerQuery = function(){
+    var queryName = "";
+    
+    $.ajax({
+        url:queryAddress,
+        method:'PUT',
+        headers:{
+            "Cache":"no-cache"
+        },
+        data:{
+            "queryName":queryName
+        }
+    })
+    .done(function(response){
+        $('#response').html(response);
+    })
+    .fail(function( jqXHR, textStatus){
+        var error = "Error: " +textStatus+"\nPlease raise you hand and wait for a tutor";
+        $('#response').html(error);
+
+    })
+}
