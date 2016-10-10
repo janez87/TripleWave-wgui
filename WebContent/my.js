8aa118cdcdeda8aef89c1df97e693c99a1c2dd08
Andrea <andrea.mauri87@gmail.com>
2016-10-10 18:02:07 +0200

    it works

--- WebContent/my.js
+++ WebContent/my.js
@@ -6,6 +6,37 @@ var sgraphAddress = "http://131.175.141.249/TripleWave-transform/sgraph";
 var observerCallback = "http://localhost:8080/triplewave-wgui/ResultWriter"
 var resultUrl = "http://localhost:8080/triplewave-wgui/ResultReader"
 
+var queries = {
+		topUserWiki:"REGISTER QUERY topUserWiki AS " +
+        "PREFIX prov:<http://www.w3.org/ns/prov#> " +
+        "PREFIX sc:<https://schema.org/> " +
+        "SELECT ?agent (COUNT(?t) AS ?c) " +
+        "FROM STREAM <http://131.175.141.249/TripleWave-transform/sgraph> [RANGE 10s STEP 10s] " +
+        "WHERE { " +
+        "?t sc:agent ?agent " +
+        "} " +
+        "GROUP BY ?agent " +
+        "ORDER BY desc(?c) " +
+        "LIMIT 15",
+        
+		topPageWiki:""
+};
+
+var load = function(query){
+	
+	var queryBody = queries[query]
+	
+	$("#query")[0].value=queryBody;
+	console.log(queryBody)
+	$("#queryName")[0].value = query;
+	
+}
+
+var clearFields = function(){
+	console.log("clearing")
+	$("#query").html("")
+	$("#queryName")[0].value = "";
+}
 var initStream = function () {
     var data = [];
 
@@ -42,7 +73,7 @@ var registerStream = function () {
         }
     })
         .done(function (response) {
-            $('#response').html(response);
+          
         })
         .fail(function (jqXHR, textStatus) {
             console.log(jqXHR)
@@ -56,22 +87,21 @@ var registerStream = function () {
 }
 
 
+var createResultDiv = function(queryName){
+	
+	var div = $('<div class="row"><div class="col-md-10 response"><h3>'+queryName+'</h3><textarea id="'+queryName+'"></textarea></div></div>');
+	
+	$('#responseContainer').append(div);
+};
+
 var registerQuery = function () {
-    var queryName = "topUserWiki";
+    var queryName = $("#queryName")[0].value;
 
-    var queryContent = "REGISTER QUERY topUserWiki AS " +
-        "PREFIX prov:<http://www.w3.org/ns/prov#> " +
-        "PREFIX sc:<https://schema.org/> " +
-        "SELECT ?agent (COUNT(?t) AS ?c) " +
-        "FROM STREAM <http://131.175.141.249/TripleWave-transform/sgraph> [RANGE 10s STEP 10s] " +
-        "WHERE { " +
-        "?t sc:agent ?agent " +
-        "} " +
-        "GROUP BY ?agent " +
-        "ORDER BY desc(?c) " +
-        "LIMIT 15";
+    var queryContent =$('#query')[0].value.replace(/['"]+/g, '');
+    //var queryContent = queries[queryName]
 
-
+    console.log(queryContent)
+    console.log(queryName)
     $.ajax({
         url: queryAddress + '/' + queryName,
         method: 'PUT',
@@ -81,7 +111,6 @@ var registerQuery = function () {
         data: queryContent
         })
         .done(function (response) {
-            $('#response').html(response);
             console.log(response)
             
             $.ajax({
@@ -94,31 +123,43 @@ var registerQuery = function () {
                 data:observerCallback+'/'+queryName
             })
             .done(function(response){
-            	 $('#response').html(response);
+            	createResultDiv(queryName);
+            
+            	setInterval(getResults.bind(null,queryName),12000)
             })
             .fail(function (jqXHR, textStatus) {
 	            var error = "Error: " + textStatus + "\nPlease raise you hand and wait for a tutor";
-	            $('#response').html(error);
+	            $('#query').html(error);
 
             })    
         })
         .fail(function (jqXHR, textStatus) {
             var error = "Error: " + textStatus + "\nPlease raise you hand and wait for a tutor";
-            $('#response').html(error);
+            $('#query').html(error);
 
         })
     $('#query').html(queryContent);
 
 }
 
-var getResults = function(){
+var results = {};
+var getResults = function(query){
 	
+	console.log(query)
 	$.ajax({
-		url:resultUrl+'?key=topUserWiki',
+		url:resultUrl+'?key='+query,
 		method:'GET'
 	})
 	.done(function(response){
-		$('#response').html(response);
+		console.log(response);
+		if(!results[query]){
+			results[query] = []
+		}
+		
+		if(response){			
+			results[query].push(response);
+		}
+		$('#'+query).html(results[query]);
 	})
 	.fail(function (jqXHR, textStatus) {
             var error = "Error: " + textStatus + "\nPlease raise you hand and wait for a tutor";
